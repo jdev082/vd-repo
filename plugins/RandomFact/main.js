801b4521b2759e3841cd2e0485e4258e793716fb
jdev082 <willsjayden2@gmail.com>
2023-04-08 18:45:54 -0500

    fix: bug fixes

--- plugins/RandomFact/main.js
+++ plugins/RandomFact/main.js
@@ -6,13 +6,10 @@ const MessageActions = findByProps("sendMessage", "receiveMessage")
 let commands = [
 ]
 
-function getRandomFact() {
-    fetch('https://api.jaydendev.repl.co/fact')
-        .then(res => res.json())
-        .then(data => {
-            const { fact } = data[0];
-            return fact;
-        });
+const fact = async function () {
+    const response = await fetch(`https://api.jaydendev.repl.co/fact`);
+    const resp = await response.json();
+    return resp["0"]
 }
 
 commands.push(registerCommand({
@@ -20,12 +17,11 @@ commands.push(registerCommand({
     displayName: "fact",
     description: "Sends a random fact.",
     displayDescription: "Sends a random fact.",
-    // @ts-ignore
     applicationId: -1,
     inputType: 1,
     type: 1,
 
     execute: (args, ctx) => MessageActions.sendMessage(ctx.channel.id, {
-        content: getRandomFact()
+        content: fact()
     })
 }))
